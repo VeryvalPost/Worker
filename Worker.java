46365998081bd7f2dbd56f0b9cd1c6e52581d69a
VeryvalPost <112349610+VeryvalPost@users.noreply.github.com>
2022-09-10 11:59:19 +0300

    Update Worker.java

--- Worker.java
+++ Worker.java
@@ -3,21 +3,17 @@ public class Worker {
     private OnTaskDoneListener callback;
     private OnErrorListener errorCallback;
 
-    public Worker(OnTaskDoneListener callback) {
+    public Worker(OnTaskDoneListener callback, OnErrorListener errorCallback) {
         this.callback = callback;
-    }
-
-    public Worker(OnErrorListener errorCallback) {
         this.errorCallback = errorCallback;
     }
 
-    public void getDone(int i) {
-        callback.onDone("Task " + i + " is done");
-    }
-
-
-    public void getError(int i) {
-        errorCallback.onError("Task " + i + " is wrong");
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            if (i == 33) {
+                errorCallback.onError("Task " + i + " is wrong");
+            } else callback.onDone("Task " + i + " is done");
+        }
     }
 
     @FunctionalInterface
