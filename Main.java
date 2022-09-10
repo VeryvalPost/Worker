70e1b2573745ab7fc6e89a202794677d867d645e
VeryvalPost <112349610+VeryvalPost@users.noreply.github.com>
2022-09-10 12:00:00 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,19 +1,9 @@
-import java.util.function.Consumer;
-
 public class Main {
     public static void main(String args[]) {
 
         Worker.OnTaskDoneListener listener = System.out::println;
         Worker.OnErrorListener listenerError = System.out::println;
-
-        for (int i = 0; i < 100; i++) {
-            if (i != 33) {
-                Worker worker = new Worker(listener);
-                worker.getDone(i);
-            } else {
-                Worker worker = new Worker(listenerError);
-                worker.getError(i);
-            }
-        }
+        Worker worker = new Worker(listener,listenerError);
+        worker.start();
     }
 }
